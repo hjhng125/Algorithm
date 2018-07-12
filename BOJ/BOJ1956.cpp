750688ef29a26804127e789f9c45ca7e3ee5bb91
hjhng125 <33080926+hjhng125@users.noreply.github.com>
2018-07-13 04:00:13 +0900

    BOJ 운동

---
+++ BOJ/BOJ1956.cpp
@@ -0,0 +1,38 @@
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+
+#define INF 1e9
+using namespace std;
+
+typedef pair<int, int> pi;
+int v, e;
+int table[401][401];
+int main()
+{
+	scanf("%d %d", &v, &e);
+	for (int i = 1; i <= v; i++) {
+		for (int j = 1; j <= v; ++j)
+			table[i][j] = INF;
+	}
+	for (int i = 1; i <= e; ++i) {
+		int sc, en, c;
+		scanf("%d %d %d", &sc, & en, &c);
+		table[sc][en] = c;
+	}
+	for (int k = 1; k <= v; ++k) {
+		for (int i = 1; i <= v; ++i) {
+			for (int j = 1; j <= v; ++j) {
+				if (table[i][j] > table[i][k] + table[k][j])
+					table[i][j] = table[i][k] + table[k][j];
+			}
+		}
+	}
+	int ans = INF;
+	for (int i = 1; i <= v; ++i) {
+		ans = min(ans,table[i][i]);
+	}
+	if (ans == INF)
+		printf("-1");
+	else printf("%d", ans);
+}
