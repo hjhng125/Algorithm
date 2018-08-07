5de0f75f8861a2422092acaba664307f9b55e02b
hjhng125 <33080926+hjhng125@users.noreply.github.com>
2018-08-08 00:36:14 +0900

    BOJ 시험 성적

---
+++ BOJ/if-else chain practice/boj9498.cpp
@@ -0,0 +1,18 @@
+#include<cstdio>
+
+using namespace std;
+
+int main() {
+	int s;
+	scanf("%d", &s);
+	if (s >= 90)
+		printf("A");
+	else if (s >= 80)
+		printf("B");
+	else if (s >= 70)
+		printf("C");
+	else if (s >= 60)
+		printf("D");
+	else 
+		printf("F");
+}
