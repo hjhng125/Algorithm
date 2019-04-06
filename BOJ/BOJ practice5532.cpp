1502bed6200ce94f6429f753a7cc8bf97133d369
James <ed90023@nhnedu.com>
2019-04-06 19:28:25 +0900

    BOJ 방학 숙제

---
+++ BOJ/BOJ practice5532.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+
+    int l, a, b, c, d;
+    cin >> l >> a >> b >> c >> d;
+
+    float result1 = (float) a / c, result2 = (float) b / d;
+    if (result1 < result2)
+        result1 = result2;
+    cout << l - ceil(result1);
+}
+
+
